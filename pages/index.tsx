d8a2ad291820dee0d5453cf86b56b5144421aaa6
grzeznx2 <grzeznx2@gmail.com>
2023-06-14 21:44:18 +0200

    Move hooks from pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { useEffect, useState } from "react";
-import { useIsMounted } from "./hooks/useIsMounted";
+import { useIsMounted } from "../hooks/useIsMounted";
 import Image from "next/image";
 import HotpotSVG from "../public/images/hotpot_text.svg";
 import Link from "next/link";
